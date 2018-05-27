50406e68f872c6b2b2fe7d8f4f783983b1d851b6
Ihor Didyk <ihordidyk95@gmail.com>
2018-05-27 18:35:14 +0300

    dev: created model classes

---
+++ src/main/java/com/auth/demo/model/User.java
@@ -0,0 +1,33 @@
+package com.auth.demo.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Size(min = 4, max = 255, message = "Minimum username length: 4 characters")
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Size(min = 8, message = "Minimum password length: 8 characters")
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    List<Role> roles;
+}
