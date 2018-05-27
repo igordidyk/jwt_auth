d4aefdf75e5c8f50f23c3e9cde9774f4b91fdef0
Ihor Didyk <ihordidyk95@gmail.com>
2018-05-27 17:53:57 +0300

    dev: created proejct and configured application.yml

---
+++ src/main/java/com/auth/demo/JwtAuthDemoApplication.java
@@ -0,0 +1,12 @@
+package com.auth.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtAuthDemoApplication.class, args);
+    }
+}
